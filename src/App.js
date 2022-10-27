a55eb5060c26121804eef6f669d249be3d5056b5
hlin <gywjd5126@gmail.com>
2022-10-28 00:13:39 +0900

    mobile css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 "use strict";
 
 let count = 0;
-let cartNum = 0;
+let id = 0;
 let selectedImg = "product-1";
 let selectedOverlayImg = "product-1";
+
+class List {
+  constructor(id, productName, price, ea) {
+    this.id = id;
+    this.productName = productName;
+    this.price = price;
+    this.ea = ea;
+  }
+}
+const dataList = [];
 
 // cart
 let num = document.querySelector(".count_num");
 const cartBtn = document.querySelector(".cart_btn");
-const countMinusBtn = document.querySelector(".count_minus");
-const countPlusBtn = document.querySelector(".count_plus");
 const addBtn = document.querySelector(".btns_add");
+const cart = document.querySelector(".cart_list");
 
 // main
+const countMinusBtn = document.querySelector(".count_minus");
+const countPlusBtn = document.querySelector(".count_plus");
 const mainImg = document.querySelector(".images_main");
 const thumbForm = document.querySelector(".thumbnails");
 const thumbRadio = document.querySelectorAll(".thumb-radio");
@@ -45,28 +56,28 @@ function decreaseCount() {
 }
 
 // add the item list to shopping cart
-function makeLi(li, num) {
+function makeLi(li, data) {
   return (li.innerHTML = `
-  <li class="cart_item">
+  <li class="cart_item" key=${data.id} >
     <div class="item_info">
       <img
         src="images/image-product-1-thumbnail.jpg"
         alt="item_img"
         class="item_img"
       />
-      <span class="item_product">스니커즈</span>
-      <span class="item_price">125,000원</span>
+      <span class="item_product">${data.productName}</span>
+      <span class="item_price">${data.price}원</span>
     </div>
     <div class="cart_btns">
       <button class="count-btn cart_minus">
-        <i class="fa-solid fa-minus"></i>
+        <i class="fa-solid fa-minus" data-id=${data.id} data-type="minus"></i>
       </button>
-      <span class="cart_num">${num}</span>
+      <span class="cart_num ea_${data.id}" >${data.ea}</span>
       <button class="count-btn cart_plus">
-        <i class="fa-solid fa-plus"></i>
+        <i class="fa-solid fa-plus"  data-id=${data.id} data-type="plus"></i>
       </button>
       <button class="count-btn cart_remove">
-        <i class="fa-solid fa-trash-can"></i>
+        <i class="fa-solid fa-trash-can" data-id=${data.id} data-type="remove"></i>
       </button>
     </div>
   </li>
@@ -75,12 +86,14 @@ function makeLi(li, num) {
 
 function addCart() {
   if (count === 0) {
-    alert("수량을 추가해주세요.");
+    return alert("수량을 추가해주세요.");
   }
   if (count > 0 && window.confirm("추가하시겠습니까?")) {
     const cart = document.querySelector(".cart_list");
     const empty = document.querySelector(".cart_empty");
     const li = document.createElement("li");
+
+    let cartNum = 0;
 
     //copy the count
     cartNum += count;
@@ -88,20 +101,41 @@ function addCart() {
     //hide the empty message
     empty.classList.add("hidden");
 
-    //make and append item list
-    makeLi(li, cartNum);
+    // make the cart list and push to array
+    const list = new List(id, "Fall Sneakers", "125000", cartNum);
+    dataList.push(list);
 
+    //make and append li to cart list
+    makeLi(li, list);
     cart.appendChild(li);
 
-    const minusBtn = document.querySelector(".cart_minus");
-    const plusBtn = document.querySelector(".cart_plus");
-    const cartEa = document.querySelector(".cart_num");
+    // cart 수량, 삭제버튼 작동시키기
 
-    minusBtn.addEventListener("click", () => (cartNum -= 1));
-    plusBtn.addEventListener("click", () => (cartNum += 1));
-    cartEa.innerText = cartNum;
+    // 초기화, 값 출력
     count = 0;
+    id++;
     num.innerText = count;
+  }
+}
+
+function cartBtnsHandler(e) {
+  const data = e.target.dataset;
+  const dataId = data.id;
+  const dataType = data.type;
+  const div = e.target.parentElement.parentElement;
+  const span = div.children[1];
+  console.log(div, span);
+  console.log(span.value);
+  if (!dataId) {
+    return;
+  } else if (dataType === "plus") {
+    // dataList.map((list) =>
+    //   list.id === parseInt(dataId)
+    //     ? (list.ea += 1 )
+    //     : ""
+    // );
+    // span.innerText =
+    // const datahi = dataList.find((it) => it.id === parseInt(dataId));
   }
 }
 
@@ -203,6 +237,7 @@ function addEvent() {
   thumbForm.addEventListener("click", changeMainImg);
   overlayThumbForm.addEventListener("click", overlayThumbHandler);
   overlayBtns.addEventListener("click", overlaySliderBtnsHandler);
+  cart.addEventListener("click", cartBtnsHandler);
 }
 
 addEvent();
